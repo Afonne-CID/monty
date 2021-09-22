74303e2ad88174ca9f3859cbba830d51f5f13e94
Afonne-CID <delightinbusiness@gmail.com>
2021-09-22 06:52:30 -0700

    commit

---
+++ funcs2.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * adds - sums the top two values of a doubly linked list
+ * @stack: a pointer to the top node of a doubly linked list
+ * @line_number: the current line number of the file we are wroking on
+ */
+void _add(stack_t **stack, unsigned int line_number)
+{
+	if ((*stack)->next == NULL || (*stack)->next == NULL)
+	{
+		fprintf("L%d: usage: push integer
+	}
+}
--- monty.h
+++ monty.h
@@ -32,7 +32,12 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-void push(stack_t **stack, unsigned int line_number);
-void pall(stack_t **stack, unsigned int line_number);
+void (*get_op_func(char *opcode))(stack_t **, unsigned int);
+void _push(stack_t **stack, unsigned int line_number);
+void _pall(stack_t **stack, unsigned int line_number);
+void _pint(stack_t **stack, unsigned int line_number);
+void _pop(stack_t **stack, unsigned int line_number);
+void _swap(stack_t **stack, unsigned int line_number);
+void _add(stack_t **stack, unsigned int line_number);
 
 #endif /* MONTY_H */
