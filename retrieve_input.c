d6713b6bbf88eb74ab1ae1b40b85d64a9d0575c5
Afonne-CID <delightinbusiness@gmail.com>
2021-09-23 08:18:10 -0700

    bulk commit

---
+++ retrieve_input.c
@@ -0,0 +1,42 @@
+#include "shell.h"
+
+/**
+ * retrieve_input - gets line(s) of input
+ *
+ * Return: the retrieved string
+ */
+char *retrieve_input(void)
+{
+	ssize_t get_cnt;
+	size_t cnt;
+	char *next = NULL;
+	char *buffer = NULL;
+
+	get_cnt = getline(&buffer, &cnt, stdin);
+	if (get_cnt == -1)
+	{
+		_putchar(10);
+		free(buffer);
+		exit(EXIT_FAILURE);
+	}
+	buffer[cnt - 1] = '\0';
+	while (buffer[_strlen(buffer) - 2] == '\\')
+	{
+		buffer[_strlen(buffer) - 2] = '\0';
+		next = buf_size(next, buffer);
+		_strcat(next, buffer);
+		prompt2();
+		get_cnt = getline(&buffer, &cnt, stdin);
+		if (get_cnt == -1)
+		{
+			_putchar(10);
+			free(buffer);
+			free(next);
+			exit(EXIT_FAILURE);
+		}
+	}
+	next = buf_size(next, buffer);
+	_strcat(next, buffer);
+	free(buffer);
+	return (next);
+}
---
+++ tokenizer.c
@@ -0,0 +1,44 @@
+#include "shell.h"
+
+/**
+ * _tokenizer - tokenizes a string into chunks
+ * @str: the string to tokenize
+ * @delim: delimeter
+ *
+ * Return: Double pointer to string (array of arrays)
+ */
+char **_tokenizer(char *str, char *delim)
+{
+	char **token_holder, **holder, *token;
+	unsigned int length;
+	int i;
+
+	length = _strlen(str);
+	if (length == 0)
+		return (NULL);
+
+	token_holder = malloc((sizeof(char *)) * (length + 1));
+	if (token_holder == NULL)
+		return (NULL);
+
+	i = 0;
+	token = strtok(str, delim);
+	while (token != NULL)
+	{
+		token_holder[i] = malloc(_strlen(token) + 1);
+		if (token_holder[i] == NULL)
+		{
+			free_mem(token_holder);
+			return (NULL);
+		}
+
+		_strcpy(token_holder[i], token);
+		token = strtok(NULL, delim);
+		i++;
+	}
+
+	token_holder[i] = NULL;
+	holder = token_holder;
+	free_mem(token_holder);
+	return (holder);
+}
